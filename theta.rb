10092d6ccea5a13af108586e9dca4ca8cf7ccd85
Reid Netterville III <reidiiius@gmail.com>
2012-09-20 02:54:11 -0500

    'clave'

---
+++ theta.rb
@@ -0,0 +1,184 @@
+#!/usr/bin/env ruby 
+
+def nat_bata
+ ion_nat = '101011010101',
+ dor_nat = '101101010110',
+ phry_nat = '110101011010',
+ lyd_nat = '101010110101',
+ mixo_nat = '101011010110',
+ aeol_nat = '101101011010',
+ loc_nat = '110101101010' 
+end 
+
+def aug6_bata
+ ion_aug6 = '101011010011',
+ dor_aug5 = '101101001110',
+ phry_aug4 = '110100111010',
+ lyd_aug3 = '101001110101',
+ mixo_aug2 = '100111010110',
+ aeol_aug1 = '011101011010',
+ loc_aug7 = '110101101001' 
+end 
+
+def dim5_bata
+ ion_dim5 = '101011100101',
+ dor_dim4 = '101110010110',
+ phry_dim3 = '111001011010',
+ lyd_dim2 = '110010110101',
+ mixo_dim1 = '001011010111',
+ aeol_dim7 = '101101011100',
+ loc_dim6 = '110101110010' 
+end 
+
+def dim3_bata
+ ion_dim3 = '101101010101',
+ dor_dim2 = '110101010110',
+ phry_dim1 = '010101011011',
+ lyd_dim7 = '101010110110',
+ mixo_dim6 = '101011011010',
+ aeol_dim5 = '101101101010',
+ loc_dim4 = '110110101010' 
+end 
+
+def dim6_bata
+ ion_dim6 = '101011011001',
+ dor_dim5 = '101101100110',
+ phry_dim4 = '110110011010',
+ lyd_dim3 = '101100110101',
+ mixo_dim2 = '110011010110',
+ aeol_dim1 = '001101011011',
+ loc_dim7 = '110101101100' 
+end
+
+def aug5_bata
+ ion_aug5 = '101011001101',
+ dor_aug4 = '101100110110',
+ phry_aug3 = '110011011010',
+ lyd_aug2 = '100110110101',
+ mixo_aug1 = '011011010110',
+ aeol_aug7 = '101101011001',
+ loc_aug6 = '110101100110' 
+end 
+
+def dim2_bata 
+ ion_dim2 = '110011010101',
+ dor_dim1 = '001101010111',
+ phry_dim7 = '110101011100',
+ lyd_dim6 = '101010111001',
+ mixo_dim5 = '101011100110',
+ aeol_dim4 = '101110011010',
+ loc_dim3 = '111001101010' 
+end 
+
+def aug2_bata
+ ion_aug2 = '100111010101',
+ dor_aug1 = '011101010110',
+ phry_aug7 = '110101011001',
+ lyd_aug6 = '101010110011',
+ mixo_aug5 = '101011001110',
+ aeol_aug4 = '101100111010',
+ loc_aug3 = '110011101010' 
+end 
+
+def dim26_bata
+ ion_dim26 = '110011011001',
+ dor_dim15 = '001101100111',
+ phry_dim47 = '110110011100',
+ lyd_dim36 = '101100111001',
+ mixo_dim25 = '110011100110',
+ aeol_dim14 = '001110011011',
+ loc_dim37 = '111001101100' 
+end 
+
+def dim23_bata
+ ion_dim23 = '110101010101',
+ dor_dim12 = '010101010111',
+ phry_dim17 = '010101011101',
+ lyd_dim67 = '101010111010',
+ mixo_dim56 = '101011101010',
+ aeol_dim45 = '101110101010',
+ loc_dim34 = '111010101010' 
+end 
+
+def dim34_bata
+ ion_dim34 = '101110010101',
+ dor_dim23 = '111001010110',
+ phry_dim12 = '100101011011',
+ lyd_dim17 = '001010110111',
+ mixo_dim67 = '101011011100',
+ aeol_dim56 = '101101110010',
+ loc_dim45 = '110111001010' 
+end
+
+def aug17_bata
+ ion_aug17 = '111011010100',
+ dor_aug67 = '101101010011',
+ phry_aug56 = '110101001110',
+ lyd_aug45 = '101010011101',
+ mixo_aug34 = '101001110110',
+ aeol_aug23 = '100111011010',
+ loc_aug12 = '011101101010' 
+end 
+
+def aug2dim6_bata
+ ion_aug2dim6 = '100111011001',
+ dor_aug1dim5 = '011101100110',
+ phry_dim4aug7 = '110110011001',
+ lyd_dim3aug6 = '101100110011',
+ mixo_dim2aug5 = '110011001110',
+ aeol_dim1aug4 = '001100111011',
+ loc_aug3dim7 = '110011101100'
+end 
+
+def dim2aug5_bata
+ ion_dim2aug5 = '110011001101',
+ dor_dim1aug4 = '001100110111',
+ phry_aug3dim7 = '110011011100',
+ lyd_aug2dim6 = '100110111001',
+ mixo_aug1dim5 = '011011100110',
+ aeol_dim4aug7 = '101110011001',
+ loc_dim3aug6 = '111001100110' 
+end 
+
+puts 
+puts ' nat_bata'
+puts nat_bata 
+puts 
+puts ' aug6_bata'
+puts aug6_bata 
+puts
+puts ' dim5_bata'
+puts dim5_bata 
+puts
+puts ' dim3_bata'
+puts dim3_bata
+puts
+puts ' dim6_bata'
+puts dim6_bata 
+puts
+puts ' aug5_bata'
+puts aug5_bata 
+puts
+puts ' dim2_bata'
+puts dim2_bata 
+puts 
+puts ' aug2_bata'
+puts aug2_bata 
+puts 
+puts ' dim26_bata'
+puts dim26_bata 
+puts 
+puts ' dim23_bata'
+puts dim23_bata 
+puts 
+puts ' dim34_bata'
+puts dim34_bata 
+puts 
+puts ' aug17_bata'
+puts aug17_bata 
+puts 
+puts 'aug2dim6_bata'
+puts aug2dim6_bata 
+puts 
+puts 'dim2aug5_bata'
+puts dim2aug5_bata 
