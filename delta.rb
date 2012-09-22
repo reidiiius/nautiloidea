3eceb578fd1d91dd41fa6eff89a4b0782a85ea31
Reid Netterville III <reidiiius@gmail.com>
2012-09-22 00:36:51 -0500

    update

--- delta.rb
+++ delta.rb
@@ -1,96 +1,5 @@
 #!/usr/bin env ruby 
 
- an0 = 21
- bj0 = 22 
- bn0 = 23
- cn1 = 24
- dj1 = 25,
- dn1 = 26
- ej1 = 27
- en1 = 28
- fn1 = 29
- gj1 = 30
- gn1 = 31
- aj1 = 32
- an1 = 33
- bj1 = 34
- bn1 = 35
- cn2 = 36
- dj2 = 37
- dn2 = 38
- ej2 = 39
- en2 = 40
- fn2 = 41
- gj2 = 42
- gn2 = 43
- aj2 = 44
- an2 = 45
- bj2 = 46
- bn2 = 47
- cn3 = 48
- dj3 = 49
- dn3 = 50
- ej3 = 51
- en3 = 52
- fn3 = 53
- gj3 = 54
- gn3 = 55
- aj3 = 56
- an3 = 57
- bj3 = 58
- bn3 = 59
- cn4 = 60
- dj4 = 61
- dn4 = 62
- ej4 = 63
- en4 = 64
- fn4 = 65
- gj4 = 66
- gn4 = 67
- aj4 = 68
- an4 = 69
- bj4 = 70
- bn4 = 71
- cn5 = 72
- dj5 = 73
- dn5 = 74
- ej5 = 75
- en5 = 76
- fn5 = 77
- gj5 = 78
- gn5 = 79
- aj5 = 80
- an5 = 81
- bj5 = 82
- bn5 = 83
- cn6 = 84
- dj6 = 85
- dn6 = 86
- ej6 = 87
- en6 = 88
- fn6 = 89
- gj6 = 90
- gn6 = 91
- aj6 = 92
- an6 = 93
- bj6 = 94
- bn6 = 95
- cn7 = 96
- dj7 = 97
- dn7 = 98
- ej7 = 99
- en7 = 100
- fn7 = 101
- gj7 = 102
- gn7 = 103
- aj7 = 104
- an7 = 105
- bj7 = 106
- bn7 = 107
- cn8 = 108 
-
- # double_fifths
-
 def sus7
  [21, 26, 28, 31,
   33, 38, 40, 43,
@@ -123,8 +32,6 @@ def maj7
   93, 97, 100,104,
   105,106] 
 end 
-
- # fifth_tritone 
  
 def maj7_aug4 
  [21, 25, 27, 32,
@@ -158,8 +65,6 @@ def dom7
   93, 97, 100,103,
   105,106] 
 end 
-
- # double_tritones
  
 def dom7_dim5 
  [21, 25, 27, 31,
@@ -214,9 +119,7 @@ def min_aug4
   81, 84, 87, 88,
   93, 96, 99, 100,
   105,108]  
-end
-
- # augmented 
+end 
  
 def maj7_aug5 
  [21, 25, 29, 32,
@@ -250,7 +153,7 @@ def dom7_aug5
   93, 97, 101,103,
   105,106] 
 end 
- 
+
 chord_list = [
 ' sus7',
 ' min7',
@@ -260,7 +163,7 @@ chord_list = [
 ' dom7', 
 ' dom7-5',
 ' dim6',
-' 13+9', 
+' funk', 
 ' maj-2',
 ' min+4',
 ' maj7+5',
@@ -268,10 +171,10 @@ chord_list = [
 ' dom7+5' ]
 
 puts 
-puts 'For Table of Contents,'
+puts 'For the Table of Contents,'
 puts 'type the word "index" or "list".'
 puts 'To exit and leave the program,'
-puts 'type the words "exit" or "quit".'
+puts 'type the word "exit" or "quit".'
 
 x_status = false
 while (not x_status)
@@ -296,7 +199,7 @@ select_chord = gets.chomp
   puts dom7_dim5[16..24].reverse 
  elsif select_chord == 'dim6' 
   puts dim_6[16..24].reverse 
- elsif select_chord == '13+9' 
+ elsif select_chord == 'funk' 
   puts dim2aug4[16..24].reverse 
  elsif select_chord == 'maj-2' 
   puts maj_dim2[16..24].reverse 
