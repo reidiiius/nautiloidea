839ed42649dc2a81ca29fc5a9d3f10d99d07bc15
Reid Netterville III <reidiiius@gmail.com>
2012-10-25 20:21:21 -0500

    twins

--- lib/nautiloidea/zodiac/gemini.rb
+++ lib/nautiloidea/zodiac/gemini.rb
@@ -5,28 +5,6 @@ module Gemini
   class Pollux             
 
     def initialize 
-	  signet_n4 
-	end 
- 
-    def signet_n4 
-      @n4 =
-      [
-      ' ___ fn6 gj6 ___ aj6 ___ bj6 cj7 ___ dj7 ___ ej7 ___ ', 
-      ' cj6 ___ dj6 ___ ej6 ___ fn6 gj6 ___ aj6 ___ bj6 cj7 ', 
-      ' ___ aj5 ___ bj5 cj6 ___ dj6 ___ ej6 ___ fn6 gj6 ___ ', 
-      ' ___ ej5 ___ fn5 gj5 ___ aj5 ___ bj5 cj6 ___ dj6 ___ ', 
-      ' ___ bj4 cj5 ___ dj5 ___ ej5 ___ fn5 gj5 ___ aj5 ___ ', 
-      ' ___ fn4 gj4 ___ aj4 ___ bj4 cj5 ___ dj5 ___ ej5 ___ '] 
-    end 
- 
-    def n4 
-	  @n4 
-    end 
-  end 
-  
-  class Castor             
-
-    def initialize 
 	  signet_n7 
 	end 
  
@@ -43,17 +21,40 @@ module Gemini
  
     def n7 
 	  @n7 
-    end  
+    end 
+
+  end 
+  
+  class Castor             
+
+    def initialize 
+	  signet_n4 
+	end 
+ 
+    def signet_n4 
+      @n4 =
+      [
+      ' ___ fn6 gj6 ___ aj6 ___ bj6 cj7 ___ dj7 ___ ej7 ___ ', 
+      ' cj6 ___ dj6 ___ ej6 ___ fn6 gj6 ___ aj6 ___ bj6 cj7 ', 
+      ' ___ aj5 ___ bj5 cj6 ___ dj6 ___ ej6 ___ fn6 gj6 ___ ', 
+      ' ___ ej5 ___ fn5 gj5 ___ aj5 ___ bj5 cj6 ___ dj6 ___ ', 
+      ' ___ bj4 cj5 ___ dj5 ___ ej5 ___ fn5 gj5 ___ aj5 ___ ', 
+      ' ___ fn4 gj4 ___ aj4 ___ bj4 cj5 ___ dj5 ___ ej5 ___ '] 
+    end 
+ 
+    def n4 
+	  @n4 
+    end   
 
   end 
 
   puts 
-  puts 'n4' 
-  puts   
-  puts Pollux.new.n4
-  puts 
   puts 'n7' 
   puts   
-  puts Castor.new.n7  
+  puts Pollux.new.n7 
+  puts 
+  puts 'n4' 
+  puts   
+  puts Castor.new.n4   
    
 end 
