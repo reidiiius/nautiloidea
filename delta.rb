5d96a08e09aa627bd98582823692a2bba3ec0a60
Reid Netterville III <reidiiius@gmail.com>
2012-10-25 19:25:06 -0500

    clean up

--- delta.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin env ruby 
-
-puts  
-puts " Nothing to see here, move along." 
- 
--- omicron.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby 
-
-puts  
-puts " This is not the file your looking for." 
-puts " Move along now, move along." 
