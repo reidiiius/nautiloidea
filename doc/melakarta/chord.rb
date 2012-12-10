fb3d8b72e652115f42bb1dc25e8e23072c1d275e
Reid Netterville III <reidiiius@gmail.com>
2012-12-10 16:36:29 -0600

    added chords

---
+++ doc/melakarta/chord.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby 
+
+require 'yaml'
+
+infodata = YAML.load_file 'eadgcf.yml'
+ puts
+ puts '(sus)' 
+ puts infodata['sus']
+ puts
+ puts
+ puts '(mi7)'  
+ puts infodata['mi7']
+ puts
+ puts
+ puts '(ma7)'  
+ puts infodata['ma7'] 
+ puts
+ puts
+ puts '(ma7k4)'  
+ puts infodata['ma7k4']
+ puts
+ puts
+ puts '(mi6)' 
+ puts infodata['mi6'] 
+ puts
+ puts 
+ puts '(dom7)' 
+ puts infodata['dom7'] 
+  puts
+ puts
+ puts '(n5j2k4)'  
+ puts infodata['n5j2k4'] 
+ puts
+ puts
+ puts '(dom7j5)'  
+ puts infodata['dom7j5']
+ puts
+ puts
+ puts '(o6)' 
+ puts infodata['o6'] 
+ puts
+ puts 
+ puts '(ok7)' 
+ puts infodata['ok7'] 
+ puts
+ puts
+ puts '(mik4)'  
+ puts infodata['mik4'] 
+ puts
+ puts
+ puts '(mik7)'  
+ puts infodata['mik7']
+ puts
+ puts
+ puts '(dom7k5)' 
+ puts infodata['dom7k5'] 
+ puts
+ puts 
+ puts '(ma7k5)' 
+ puts infodata['ma7k5'] 
+ 
--- doc/melakarta/index.rb
+++ doc/melakarta/index.rb
@@ -17,7 +17,8 @@ end
 def help 
  puts
  puts ' For a table of contents,'
- puts ' just type "index" or "list".'
+ puts ' just type "list" or "show".'
+ puts ' For chord patterns type "chord".'
  puts ' If you want to leave the program,'
  puts ' just type "exit" or "quit".'
 end  
@@ -35,37 +36,39 @@ while (not x_status)
 puts
 print ' Enter selection: '
  selection = gets.chomp 
- if (selection =~ /list|index/ or selection == 'show')
+ if (selection =~ /[Ll]ist|[Ss]how/ or selection == '0')
   puts  
-  puts index  
- elsif (selection =~ /A|aditya/ or selection == '12') 
+  puts index 
+ elsif (selection =~ /[Hh]elp/ or selection == '')
+  puts 
+  puts help   
+ elsif (selection =~ /[Aa]ditya/ or selection == '12') 
   load 'aditya.rb' 
- elsif (selection =~ /A|agni/ or selection == '3')
+ elsif (selection =~ /[Aa]gni/ or selection == '3')
   load 'agni.rb'
- elsif (selection =~ /B|bana/ or selection == '5')
+ elsif (selection =~ /[Bb]ana/ or selection == '5')
   load 'bana.rb'
- elsif (selection =~ /B|brahma/ or selection == '9')
+ elsif (selection =~ /[Bb]rahma/ or selection == '9')
   load 'brahma.rb'
- elsif (selection =~ /D|disi/ or selection == '10')
+ elsif (selection =~ /[Cc]hord|[Cc]hords/ or selection == '13')  
+  load 'chord.rb'
+ elsif (selection =~ /[Dd]isi/ or selection == '10')
   load 'disi.rb'
- elsif (selection =~ /I|indu/ or selection == '1')
+ elsif (selection =~ /[Ii]ndu/ or selection == '1')
   load 'indu.rb'
- elsif (selection =~ /N|netra/ or selection == '2')
+ elsif (selection =~ /[Nn]etra/ or selection == '2')
   load 'netra.rb'
- elsif (selection =~ /R|rishi/ or selection == '7')
+ elsif (selection =~ /[Rr]ishi/ or selection == '7')
   load 'rishi.rb'
- elsif (selection =~ /R|ritu/ or selection == '6')
+ elsif (selection =~ /[Rr]itu/ or selection == '6')
   load 'ritu.rb'
- elsif (selection =~ /R|rudra/ or selection == '11')
+ elsif (selection =~ /[Rr]udra/ or selection == '11')
   load 'rudra.rb'
- elsif (selection =~ /V|vasu/ or selection == '8')
+ elsif (selection =~ /[Vv]asu/ or selection == '8')
   load 'vasu.rb'
- elsif (selection =~ /V|veda/ or selection == '4')
+ elsif (selection =~ /[Vv]eda/ or selection == '4')
   load 'veda.rb'
- elsif (selection =~ /x|q/ or selection == '0')
-  x_status = true 
- elsif selection =~ /^exit|quit/ 
- exit 
- else puts help 
+ elsif (selection =~ /[Ee]xit|[Qq]uit/ or selection == 'end')
+  x_status = true  
  end
 end 
