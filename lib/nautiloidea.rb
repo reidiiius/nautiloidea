f726d215ec109f331d8defa83eb41579facd2d63
Reid Netterville III <reidiiius@gmail.com>
2012-12-31 01:29:45 -0600

    prune

--- lib/nautiloidea.rb
+++ lib/nautiloidea.rb
@@ -2,13 +2,12 @@
 
 dir = File.dirname(__FILE__) + '/nautiloidea'
  require dir + '/lambda'
- require dir + '/alpha' 
- require dir + '/gamma'   
- require dir + '/tau' 
+ require dir + '/alpha'
+ require dir + '/gamma'
  require dir + '/sigma'
- require dir + '/zodiac/lyra' 
+ require dir + '/zodiac/lyra'
  require dir + '/zodiac/draco'
- require dir + '/epsilon' 
+ require dir + '/epsilon'
  require dir + '/theta'
  require dir + '/psi'
 
