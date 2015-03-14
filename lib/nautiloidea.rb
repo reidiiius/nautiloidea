b67ac42e29f3887a5807fb0a7e5a774fd7004a99
Reid Netterville III <reidiiius@gmail.com>
2015-03-13 19:24:02 -0500

    150313.0

--- lib/nautiloidea.rb
+++ lib/nautiloidea.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/ruby 
 
 dir = File.dirname(__FILE__) + '/nautiloidea'
- require dir + '/lambda'
-# require dir + '/alpha'
-# require dir + '/gamma'
- require dir + '/sigma'
- require dir + '/zodiac/lyra'
- require dir + '/zodiac/draco'
- require dir + '/epsilon'
- require dir + '/lapis'
- require dir + '/theta'
- require dir + '/psi'
+ require dir + '/dev/lambda'
+# require dir + '/dev/alpha'
+# require dir + '/dev/gamma'
+# require dir + '/dev/sigma'
+# require dir + '/zodiac/lyra'
+# require dir + '/zodiac/draco'
+# require dir + '/dev/epsilon'
+# require dir + '/lapis'
+ require dir + '/sunstone'
+ require dir + '/dev/theta'
+ require dir + '/dev/psi'
 
- 
---
+++ lib/nautiloidea/sunstone.rb
@@ -0,0 +1,677 @@
+#!/usr/bin/ruby
+
+require_relative "signature.rb"
+
+n0 = Proc.new do
+  str = GemStone::N0
+    puts "\nn0"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+k6 = Proc.new do
+  str = GemStone::K6
+    puts "\nk6"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j17 = Proc.new do
+  str = GemStone::J17
+    puts "\nj17"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+k6x5 = Proc.new do
+  str = GemStone::K6X5
+    puts "\nk6x5"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j17y2 = Proc.new do
+  str = GemStone::J17Y2
+    puts "\nj17y2"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j3 = Proc.new do
+  str = GemStone::J3
+    puts "\nj3"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j34k6 = Proc.new do
+  str = GemStone::J34K6
+    puts "\nj34k6"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j17k2 = Proc.new do
+  str = GemStone::J17K2
+    puts "\nj17k2"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+n26y5 = Proc.new do
+  str = GemStone::N26Y5
+    puts "\nn26y5"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+k26x5 = Proc.new do
+  str = GemStone::K26X5
+    puts "\nk26x5"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j6 = Proc.new do
+  str = GemStone::J6
+    puts "\nj6"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j36 = Proc.new do
+  str = GemStone::J36
+    puts "\nj36"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+k56 = Proc.new do
+  str = GemStone::K56
+    puts "\nk56"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j136y7 = Proc.new do
+  str = GemStone::J136Y7
+    puts "\nj136y7"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+k56x4 = Proc.new do
+  str = GemStone::K56X4
+    puts "\nk56x4"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+n167x4 = Proc.new do
+  str = GemStone::N167X4
+    puts "\nn167x4"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j3k5x4 = Proc.new do
+  str = GemStone::J3K5X4
+    puts "\nj3k5x4"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j167y2 = Proc.new do
+  str = GemStone::J167Y2
+    puts "\nj167y2"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j2k56x4 = Proc.new do
+  str = GemStone::J2K56X4
+    puts "\nj2k56x4"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+k157x6 = Proc.new do
+  str = GemStone::K157X6
+    puts "\nk157x6"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+k1j6 = Proc.new do
+  str = GemStone::K1J6
+    puts "\nk1j6"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+n345 = Proc.new do
+  str = GemStone::N345
+    puts "\nn345"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j2 = Proc.new do
+  str = GemStone::J2
+    puts "\nj2"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j26 = Proc.new do
+  str = GemStone::J26
+    puts "\nj26"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j236 = Proc.new do
+  str = GemStone::J236
+    puts "\nj236"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j23 = Proc.new do
+  str = GemStone::J23
+    puts "\nj23"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j23k6 = Proc.new do
+  str = GemStone::J23K6
+    puts "\nj23k6"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j2y3 = Proc.new do
+  str = GemStone::J2Y3
+    puts "\nj2y3"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j2k6 = Proc.new do
+  str = GemStone::J2K6
+    puts "\nj2k6"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j26y3 = Proc.new do
+  str = GemStone::J26Y3
+    puts "\nj26y3"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j2k56 = Proc.new do
+  str = GemStone::J2K56
+    puts "\nj2k56"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j246y3 = Proc.new do
+  str = GemStone::J246Y3
+    puts "\nj246y3"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j26y34 = Proc.new do
+  str = GemStone::J26Y34
+    puts "\nj26y34"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j2k6x5 = Proc.new do
+  str = GemStone::J2K6X5
+    puts "\nj2k6x5"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j2k6y3 = Proc.new do
+  str = GemStone::J2K6Y3
+    puts "\nj2k6y3"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j3k6 = Proc.new do
+  str = GemStone::J3K6
+    puts "\nj3k6"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+n45y2 = Proc.new do
+  str = GemStone::N45Y2
+    puts "\nn45y2"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+j3k56x4 = Proc.new do
+  str = GemStone::J3K56X4
+    puts "\nj3k56x4"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+k2j6 = Proc.new do
+  str = GemStone::K2J6
+    puts "\nk2j6"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+n5y2 = Proc.new do
+  str = GemStone::N5Y2
+    puts "\nn5y2"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+k26 = Proc.new do
+  str = GemStone::K26
+    puts "\nk26"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+k256 = Proc.new do
+  str = GemStone::K256
+    puts "\nk256"
+    puts str[:fn]
+    puts str[:cn]
+    puts str[:gn]
+    puts str[:dn]
+    puts str[:an]
+    puts str[:en]
+    puts str[:bn]
+end
+
+def help
+  puts
+  puts " help  -h    This help message"
+  puts " list  -l    List of objects" 
+  puts " next  -n    Move to next section"
+  puts " quit  -q    Leave the program"
+end
+
+index = [
+    "n0",     "k6",   "j17",  "k6x5",  "j17y2",    "j3", 
+ "j34k6",  "j17k2", "n26y5", "k26x5",     "j6",   "j36", 
+   "k56", "j136y7", "k56x4","n167x4", "j3k5x4","j167y2", 
+    "j2",   "j236",   "j26",   "j23",  "j23k6",  "j2y3", 
+  "j2k6",  "j26y3", "j2k56","j246y3","j2k56x4","k157x6", 
+"j26y34", "j2k6x5","j2k6y3",  "k1j6",   "n345",  "j3k6", 
+ "n45y2","j3k56x4",  "k2j6",  "n5y2",    "k26",  "k256"]
+
+x_status = false
+
+  puts "\n Basic Commands"
+  puts help
+
+while ( not x_status )
+   puts
+   print "Enter selection: "
+   signet = gets.chomp
+
+  if ( signet =~ /help/ or signet == "-h" )
+    puts help
+
+  elsif ( signet =~ /ndex|list/ or signet == "-l" )
+    puts index
+
+  elsif ( signet =~ /next/ or signet == "-n" )
+    x_status = true
+
+  elsif ( signet =~ /exit|quit/ or signet == "-q" )
+    exit
+
+  elsif signet == "n0"
+    n0.call
+
+  elsif signet == "k6"
+    k6.call
+
+  elsif signet == "j17"
+    j17.call
+
+  elsif signet == "k6x5"
+    k6x5.call
+
+  elsif signet == "j17y2"
+    j17y2.call
+
+  elsif signet == "j3"
+    j3.call
+
+  elsif signet == "j34k6"
+    j34k6.call
+
+  elsif signet == "j17k2"
+    j17k2.call
+
+  elsif signet == "n26y5"
+    n26y5.call
+
+  elsif signet == "k26x5"
+    k26x5.call
+
+  elsif signet == "j6"
+    j6.call
+
+  elsif signet == "j36"
+    j36.call
+
+  elsif signet == "k56"
+    k56.call
+
+  elsif signet == "j136y7"
+    j136y7.call
+
+  elsif signet == "k56x4"
+    k56x4.call
+
+  elsif signet == "n167x4"
+    n167x4.call
+
+  elsif signet == "j3k5x4"
+    j3k5x4.call
+
+  elsif signet == "j167y2"
+    j167y2.call
+
+  elsif signet == "j2k56x4"
+    j2k56x4.call
+
+  elsif signet == "k157x6"
+    k157x6.call
+
+  elsif signet == "k1j6"
+    k1j6.call
+
+  elsif signet == "n345"
+    n345.call
+
+  elsif signet == "j2"
+    j2.call
+
+  elsif signet == "j26"
+    j26.call
+
+  elsif signet == "j236"
+    j236.call
+
+  elsif signet == "j23"
+    j23.call
+
+  elsif signet == "j23k6"
+    j23k6.call
+
+  elsif signet == "j2y3"
+    j2y3.call
+
+  elsif signet == "j2k6"
+    j2k6.call
+
+  elsif signet == "j26y3"
+    j26y3.call
+
+  elsif signet == "j2k56"
+    j2k56.call
+
+  elsif signet == "j246y3"
+    j246y3.call
+
+  elsif signet == "j26y34"
+    j26y34.call
+
+  elsif signet == "j2k6x5"
+    j2k6x5.call
+
+  elsif signet == "j2k6y3"
+    j2k6y3.call
+
+  elsif signet == "j3k6"
+    j3k6.call
+
+  elsif signet == "n45y2"
+    n45y2.call
+
+  elsif signet == "j3k56x4"
+    j3k56x4.call
+
+  elsif signet == "k2j6"
+    k2j6.call
+
+  elsif signet == "n5y2"
+    n5y2.call
+
+  elsif signet == "k26"
+    k26.call
+
+  elsif signet == "k256"
+    k256.call
+
+  end
+
+end
+
