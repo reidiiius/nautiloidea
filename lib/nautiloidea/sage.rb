26644bcbc2357906bd0f71a3634d78f206630efc
Reid Netterville III <reidiiius@gmail.com>
2015-03-16 12:32:09 -0500

    150316.1

--- lib/nautiloidea/sage.rb
+++ lib/nautiloidea/sage.rb
@@ -4,13 +4,21 @@ require 'yaml'
 
 str = YAML.load_file 'crucible/lapis.yml'
 
-puts "\nn0"
-puts str['n0']['fn'][ 0,30] << str['n0']['fn'][30,60]
-puts str['n0']['cn'][ 0,35] << str['n0']['cn'][35,60]
-puts str['n0']['gn'][ 0,40] << str['n0']['gn'][40,60]
-puts str['n0']['dn'][ 0,45] << str['n0']['dn'][45,60]
-puts str['n0']['an'][ 0,50] << str['n0']['an'][50,60]
-puts str['n0']['en'][ 0,55] << str['n0']['en'][55,60]
-puts str['n0']['bn'][ 0,60]
+puts "\nn0" + " STD"
+puts str['n0']['en'][ 0,20]
+puts str['n0']['bn'][ 0,20]
+puts str['n0']['gn'][ 0,20]
+puts str['n0']['dn'][ 0,20]
+puts str['n0']['an'][ 0,20]
+puts str['n0']['en'][ 0,20]
+
+puts "\nn0" + " M3T"
+puts str['n0']['dn'][ 0,60]
+puts str['n0']['an'][ 5,60] << str['n0']['an'][ 0, 5]
+puts str['n0']['fn'][ 5,60] << str['n0']['fn'][ 0, 5]
+puts str['n0']['dn'][ 0,60]
+puts str['n0']['an'][ 5,60] << str['n0']['an'][ 0, 5]
+puts str['n0']['fn'][ 5,60] << str['n0']['fn'][ 0, 5]
+
 puts
 
